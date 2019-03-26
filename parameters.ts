400c1c3b811f2459c32dd2478fd3e05eecfd0c5c
Raul <raulito.lima.93@gmail.com>
2019-03-26 09:19:28 -0400

    fix

--- parameters.ts
+++ parameters.ts
@@ -1,6 +1,6 @@
 export class Parameters {
-    // API_URL = `https://api.2stepreviews.com/`;
-    API_URL = `https://api.dev.2stepreviewsapp.com/app_dev.php/`;
+    API_URL = `https://api.2stepreviews.com/`;
+    // API_URL = `https://api.dev.2stepreviewsapp.com/app_dev.php/`;
     // API_URL = `http://localhost/reviews/web/app_dev.php/`;
     // API_URL = `http://192.168.1.108/reviews/api/web/app_dev.php/`;
 }
